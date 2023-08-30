af1e495a2c5b7d4c1a99a613f31549a396c5e84a
MaximilianMcC <max@maximilian.co.nz>
2023-08-31 07:38:28 +1000

    Full words can be drawn in boxes

--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <ctype.h>
 #include <tice.h>
 #include <graphx.h>
 #include <sys/timers.h>
@@ -9,33 +10,57 @@ int main(void) {
 	int count = 0;
 
 	// Main loop
-	while (os_GetCSC() != sk_Clear) {
+	while (true) {
 
 		// Get keyboard input
 		uint8_t key = os_GetCSC();
 
-		// Check for what key was pressed
-		if (key == sk_Add) count++;
-		else if (key == sk_Sub) count--;
+		// Check for if they want to exit
+		if (key == sk_Clear) break;
 
 
-		// Clear screen for drawing next frame
-		gfx_FillScreen(255);
 
 		// Turn the int to a string
 		char number[10];
 		real_t temp = os_Int24ToReal(count);
 		os_RealToStr(number, &temp, 10, 1, 0);
 
-		// Print the value
-		gfx_SetTextScale(2, 2);
-		gfx_PrintStringXY(number, 25, 25);
+		char input[6] = "stray";
+
+		// Clear screen for drawing next frame
+		gfx_FillScreen(255);
+
+		// TODO: Draw a title of some sorts
+		// TODO: Instructions and information
+
+		// Draw the 6x5 boxes for the words to go into
+		short x = 0, y = 0;
+		short letterIndex = 0;
+
+		for (short i = 0; i < 6; i++)
+		{
+			y += 30;
+			for (short j = 0; j < 5; j++)
+			{
+				// Draw the box
+				gfx_Rectangle(x, y, 25, 25);
+
+				// Draw the current letter
+				gfx_SetTextXY((x + 8), (y + 8));
+				gfx_PrintChar(toupper(input[letterIndex]));
+
+				x += 30;
+				letterIndex++;
+			}
+
+			// Reset for a new column
+			x = 0;
+			letterIndex = 0;
+		}
+
 
 		// Update screen
 		gfx_SwapDraw();
-
-		// Wait to avoid skipping input
-		sleep(50);
 	}
 
 	// End
